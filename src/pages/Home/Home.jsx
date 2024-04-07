7d19e1538e56f6c7f065754a1c8f90ee8a002960
albertomargut <alberto.martinguti@gmail.com>
2024-04-07 22:12:58 +0200

    home updated and image added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,54 +13,61 @@ export const Home = () => {
             <div className="Mensaje1">
               En NutriFit, creemos firmemente en el poder transformador de la
               nutrición para mejorar la salud y el bienestar de las personas.
+              <br></br>
+              <br></br>
               Nuestra historia es una de pasión por ayudar a otros a alcanzar su
               máximo potencial y vivir vidas más saludables y felices. Fundada
               hace más de una década por un equipo de expertos en salud y
               nutrición, NutriFit nació de la visión de crear un lugar donde las
               personas pudieran acceder a orientación y apoyo nutricional
               personalizado, adaptado a sus necesidades únicas y estilo de vida.
+              <br></br>
+              <br></br>
               Desde nuestros humildes comienzos como una pequeña consulta
               nutricional, hemos crecido y evolucionado hasta convertirnos en un
               referente en el campo de la nutrición, con una amplia gama de
               servicios diseñados para satisfacer las necesidades de una
               variedad de clientes, desde atletas de élite hasta aquellos que
-              buscan mejorar su salud general. En NutriFit, nos enorgullece
-              ofrecer un enfoque integral para la salud y el bienestar,
-              centrándonos en la importancia de la alimentación equilibrada, el
-              ejercicio regular y un estilo de vida saludable en general.
-              Creemos que la nutrición es la base de una vida saludable y que
-              cada persona merece tener acceso a la información y el apoyo
-              necesarios para tomar decisiones informadas sobre su salud.
+              buscan mejorar su salud general.
+              <br></br>
+              <br></br>
+              En NutriFit, nos enorgullece ofrecer un enfoque integral para la
+              salud y el bienestar, centrándonos en la importancia de la
+              alimentación equilibrada, el ejercicio regular y un estilo de vida
+              saludable en general. Creemos que la nutrición es la base de una
+              vida saludable y que cada persona merece tener acceso a la
+              información y el apoyo necesarios para tomar decisiones informadas
+              sobre su salud.
+              <br></br>
+              <br></br>
               Nuestro compromiso con la excelencia se refleja en todo lo que
               hacemos, desde la calidad de nuestros servicios hasta la atención
               personalizada que brindamos a cada cliente. Trabajamos con un
               equipo de nutricionistas altamente capacitados y especializados,
               comprometidos a proporcionar la mejor atención posible y a ayudar
               a nuestros clientes a alcanzar sus metas de salud y bienestar.
+              <br></br>
+              <br></br>
               Además de nuestros servicios de consulta nutricional, NutriFit
               también ofrece una variedad de recursos educativos, incluyendo
               talleres, seminarios y materiales informativos, diseñados para
               capacitar a las personas para que tomen el control de su salud y
-              adopten hábitos de vida más saludables a largo plazo. En resumen,
-              en NutriFit estamos dedicados a transformar vidas a través de la
-              nutrición, ayudando a nuestros clientes a alcanzar su máximo
-              potencial y a vivir vidas más saludables y felices. Únete a
+              adopten hábitos de vida más saludables a largo plazo.
+              <br></br>
+              <br></br>
+              En resumen, en NutriFit estamos dedicados a transformar vidas a
+              través de la nutrición, ayudando a nuestros clientes a alcanzar su
+              máximo potencial y a vivir vidas más saludables y felices. Únete a
               nosotros en el camino hacia una vida mejor y descubre cómo la
               nutrición puede marcar la diferencia en tu vida hoy mismo.
             </div>
-            <img
-              className="Imagen1"
-              src="../../src/img/tattoo-shop.jpg"
-              alt="ImagenStudio1"
-            />
           </div>
 
           <div className="Bloque2">
-            <div className="Mensaje2"></div>
             <img
-              className="Imagen2"
-              src="../../src/img/artist.jpg"
-              alt="ImagenEstudio2"
+              className="Imagen1"
+              src="../../src/img/imagen1.png"
+              alt="ImagenNutricion1"
             />
             <br></br>
             <br></br>
